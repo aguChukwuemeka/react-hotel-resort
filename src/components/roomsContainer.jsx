5aa19d8f227e453fe000aa1108f762d5e30265a6
Orton Chukwuemeka <agusmart94@gmail.com>
2021-08-08 05:50:06 +0100

    rooms added

--- src/components/roomsContainer.jsx
+++ src/components/roomsContainer.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import RoomsFilter from "./roomsFilter";
 import RoomsList from "./roomsList";
+import Title from "./title";
+import { useSelector } from "react-redux";
+import Loading from "./loading";
 
 export default function RoomsContainer() {
+  const roomState = useSelector((state) => state.rooms);
+  console.log(roomState);
+    let { isLoading, sortedRooms, rooms } = roomState;
+  if (isLoading) return <Loading />;
   return (
-    <div>
+    <div className="rooms-list">
+      <Title title="search rooms" />
+      <div className="rooms-list-center"></div>
       <p>welcome to rooms container</p>
-      <RoomsFilter />
-      <RoomsList />
+      <RoomsFilter rooms={rooms} />
+      <RoomsList rooms={sortedRooms} />
     </div>
   );
 }
+
+// export const withRoomsConsumer = (Component) => (props) =>
+//   <RoomsConsumer>{(value) => <Component {...props} context={value} />}</RoomsConsumer>;
--- src/components/roomsList.jsx
+++ src/components/roomsList.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Room from "./room";
 
-export default function RoomsList() {
+export default function RoomsList({ rooms }) {
+  if (rooms.length === 0)
     return (
-        <div>
-            hello from room list
-        </div>
-    )
+      <div className="empty-search">
+        <h3>unfortunately no rooms matched your search parameters</h3>
+      </div>
+    );
+  return (
+    <section className="rooms-list">
+      <div className="rooms-list-center">
+        {rooms.map((item) => {
+          return <Room key={item.id} room={item} />;
+        })}
+      </div>
+    </section>
+  );
 }
